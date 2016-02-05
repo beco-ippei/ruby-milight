9ef26a8308ea66c5f08310e0f94b7662baa245b5
beco-ippei <beco.ippei@gmail.com>
2016-02-06 00:36:59 +0900

    add coveralls, CodeClimate

--- Gemfile
+++ Gemfile
@@ -2,3 +2,8 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in ruby-milight.gemspec
 gemspec
+
+group :test do
+  gem 'coveralls', require: false
+  gem 'codeclimate-test-reporter', require: false
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,17 @@
+if ENV['CI']
+  require 'coveralls'
+  require 'codeclimate-test-reporter'
+  Coveralls.wear!
+
+  SimpleCov.formatters = [
+    Coveralls::SimpleCov::Formatter,
+    CodeClimate::TestReporter::Formatter,
+  ]
+
+  SimpleCov.start
+  CodeClimate::TestReporter.start
+end
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'milight'
 
